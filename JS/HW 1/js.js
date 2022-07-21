d6ced3cef72d695d1779ce03905320a3d128b8b7
volesh <volesh2@gmail.com>
2022-07-21 21:33:55 +0300

    did js hw 1

---
+++ JS/HW 1/js.js
@@ -0,0 +1,43 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let n = 1;
+let e = 10;
+let f = -999;
+let g = 123;
+let h = 3.14;
+let i = 2.7;
+let j = 16;
+let k = true;
+let m = false;
+console.log(a, b, c, d, n, e, f, g, h, i, j ,k, m);
+
+alert(a)
+alert(b)
+document.write(`${b}.${c},${d}`)
+
+
+
+
+
+let firstName = ' Василь'
+let secondName = 'Олеш'
+let middleName = ' Васильович'
+let person = secondName + firstName + middleName
+
+
+
+
+let number = 100;
+let string = '100';
+let bool = true;
+console.log(typeof number, typeof string, typeof bool)
+
+
+
+
+let name = prompt('What is your name')
+let second = prompt('What is your second name')
+let age = prompt('How old are you?')
+console.log(`${name} ${second} - age`)
